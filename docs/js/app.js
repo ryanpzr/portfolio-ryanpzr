482ca72c46ebb524fe8060b15dbca80fdf17943a
Ryan <ryanpereiralimads@gmail.com>
2023-10-26 23:34:35 -0300

    Update_07

---
+++ docs/js/app.js
@@ -0,0 +1,40 @@
+function contact() {
+    window.open('https://api.whatsapp.com/send?phone=5548996480085');
+}
+
+function resume() {
+    window.open('https://drive.google.com/file/d/14y_FTAYxMtnvMUORlvRNl5YBDeJE2ZIP/view?usp=drive_link');
+}
+
+function irGitLP() {
+    window.open('https://github.com/ryanpzr/landing-page');
+}
+
+function irGitAL() {
+    window.open('https://github.com/ryanpzr/agulha-e-linha');
+}
+
+function irDeployAL() {
+    window.open('https://agulha-e-linha.up.railway.app/');
+}
+
+function irGitMC() {
+    window.open('https://github.com/ryanpzr/menu-mamma_carmela');
+}
+
+function irDeployMC() {
+    window.open('https://www.mammacarmela.com.br/');
+}
+
+function irGitCSU() {
+    window.open('https://github.com/ryanpzr/crud-sign-up');
+}
+
+function irDeployCSU() {
+    window.open('https://github.com/ryanpzr/crud-sign-up/archive/refs/heads/main.zip');
+}
+
+function redirect(id) {
+
+    window.location.href = "#" + id;
+}
