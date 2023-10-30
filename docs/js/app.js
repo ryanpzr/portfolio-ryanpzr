abbc4db37f075052cf33d4e396b28ac6544576f9
ryanpzr <ryanpereiralimads@gmail.com>
2023-10-30 20:26:54 -0300

    Update_13

--- docs/js/app.js
+++ docs/js/app.js
@@ -39,3 +39,54 @@ function redirect(id) {
     window.location.href = "#" + id;
 }
 
+const idiomas = {
+
+    'pt-BR': {
+        'cardapioMC': 'Cardápio Online',
+        'catalogoAL': 'Catálogo Agulha e Linha',
+        'sobreMim': 'Sobre mim'
+    },
+
+    'en-US': {
+
+        'cardapioMC': 'Menu Online',
+        'catalogoAL': 'Catalog Agulha e Linha',
+        'sobreMim': 'About me',
+
+    }
+
+
+}
+
+let currentLanguage = 'pt-BR'; 
+
+const buttonMudarIdioma = document.querySelector('.lenguage');
+const imgBR = document.getElementById('imgBr');
+const imgEUA = document.getElementById('imgEua');
+imgEUA.style.display = "none";
+
+buttonMudarIdioma.addEventListener('click', function () {
+
+    
+    if (currentLanguage === 'pt-BR') {
+        currentLanguage = 'en-US';
+        imgBR.style.display = "none";
+        imgEUA.style.display = "block";
+    } else {
+        currentLanguage = 'pt-BR';
+        imgEUA.style.display = "none";
+        imgBR.style.display = "block";
+        
+    }
+    atualizarIdioma();
+
+})
+
+function atualizarIdioma() {
+    
+    const elementosComTexto = document.querySelectorAll('[data-texto]');
+    elementosComTexto.forEach(elemento => {
+        const chave = elemento.dataset.texto;
+        elemento.textContent = idiomas[currentLanguage][chave];
+    });
+}
